911f2e81f2aa5018e5cf8ec532ae4cde84c8049b
tmp91 <placeholder@example.com>
2014-04-27 12:41:42 -0500

    stuff

---
+++ src/edu/clemson/cs/cpsc215/SimpleMail/EmailUtil.java
@@ -0,0 +1,15 @@
+package edu.clemson.cs.cpsc215.SimpleMail;
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMessage.RecipientType;
+
+public class EmailUtil {
+
+}
