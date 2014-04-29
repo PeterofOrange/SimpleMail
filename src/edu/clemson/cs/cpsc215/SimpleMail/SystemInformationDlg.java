02d2e7e5b031cc70f9900f2c926af12ac1feb990
tmp91 <placeholder@example.com>
2014-04-28 20:48:28 -0500

    sysinfo

--- src/edu/clemson/cs/cpsc215/SimpleMail/SystemInformationDlg.java
+++ src/edu/clemson/cs/cpsc215/SimpleMail/SystemInformationDlg.java
@@ -6,10 +6,6 @@ package edu.clemson.cs.cpsc215.SimpleMail;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,7 +14,6 @@ import javax.swing.JPanel;
 public class SystemInformationDlg extends JDialog {
 	
 	private static final long serialVersionUID = 1L;
-	private JButton devSend;
 	private JLabel builders, title, subtitle, general, version;
 	private JPanel layer;
 	 
@@ -45,7 +40,6 @@ public class SystemInformationDlg extends JDialog {
 		subtitle = new JLabel("Coders: ");
 		builders = new JLabel("Parker Davis, Peter Schatteman, Phillip Schneider");
 		general = new JLabel("This project is a simple email client programed in java.");
-		devSend = new JButton();
 		title.setFont(new Font("Arial", Font.PLAIN, 22));
 		this.add(title,BorderLayout.PAGE_START);
 		this.add(version,BorderLayout.CENTER);
@@ -54,14 +48,6 @@ public class SystemInformationDlg extends JDialog {
 		layer.add(subtitle,BorderLayout.PAGE_START);
 		layer.add(builders,BorderLayout.CENTER);
 		layer.add(general,BorderLayout.PAGE_END);
-		this.add(devSend, BorderLayout.CENTER);
-		devSend.setText("Send an Email to the developers");
-		devSend.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				EmailTransmissionDlg newMail = new EmailTransmissionDlg("prschat@clemson.edu, parkerd@clemson.edu, pschnei@clemson.edu");
-			}
-		});
 		this.setTitle("About this program");
 		this.pack();
 		this.setVisible(true);
